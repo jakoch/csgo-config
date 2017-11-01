04e9d5cc705cd376b079efc4dbf08300b4d2f16e
jakoch <jakoch@web.de>
2017-11-01 19:43:28 +0100

    add update script

---
+++ update.bat
@@ -0,0 +1,13 @@
+::@echo off
+
+SET CURL_BINARY="tools\curl\curl.exe"
+SET CURL=%CURL_BINARY% --insecure
+
+::  
+:: curl -o "$file" -z "$file" "$uri"
+::
+:: -o = write output to file
+:: -z = filetime-based download condition (download file only, if newer)
+:: -v = verbose
+::
+%CURL% -o "base.cfg" -z "base.cfg" "https://raw.githubusercontent.com/jakoch/csgo-config/master/base.cfg"
