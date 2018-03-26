a05775a0a5484f52f16e8e718066e0a98157ee9c
jakoch <jakoch@web.de>
2018-03-26 19:09:52 +0200

    add autoexec and repair to update

--- update.bat
+++ update.bat
@@ -10,6 +10,9 @@ SET CURL=%CURL_BINARY% --insecure
 :: -z = filetime-based download condition (download file only, if newer)
 :: -v = verbose
 ::
+%CURL% -o "autoexec.cfg" -z "autoexec.cfg" "https://raw.githubusercontent.com/jakoch/csgo-config/master/autoexec.cfg"
 %CURL% -o "team.cfg" -z "team.cfg" "https://raw.githubusercontent.com/jakoch/csgo-config/master/team.cfg"
 %CURL% -o "demos.cfg" -z "demos.cfg" "https://raw.githubusercontent.com/jakoch/csgo-config/master/demos.cfg"
 %CURL% -o "update.bat" -z "update.bat" "https://raw.githubusercontent.com/jakoch/csgo-config/master/update.bat"
+%CURL% -o "repair.bat" -z "repair.bat" "https://raw.githubusercontent.com/jakoch/csgo-config/master/repair.bat"
+
