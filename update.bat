54ab227d3daf8f0483ca8f48c4160c997a3676b9
jakoch <jakoch@web.de>
2018-03-26 19:04:37 +0200

    update the update script itself

--- update.bat
+++ update.bat
@@ -12,3 +12,4 @@ SET CURL=%CURL_BINARY% --insecure
 ::
 %CURL% -o "team.cfg" -z "team.cfg" "https://raw.githubusercontent.com/jakoch/csgo-config/master/team.cfg"
 %CURL% -o "demos.cfg" -z "demos.cfg" "https://raw.githubusercontent.com/jakoch/csgo-config/master/demos.cfg"
+%CURL% -o "update.bat" -z "update.bat" "https://raw.githubusercontent.com/jakoch/csgo-config/master/update.bat"
